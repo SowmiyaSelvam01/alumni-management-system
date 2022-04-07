5aa250a9b2532fd6f8bfdd773f0740ccb1c626cf
SowmiyaSelvam01 <sowmiyaselvam01@gmail.com>
2022-04-07 23:50:37 +0530

    dbname change commit

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
     $servername='localhost';
     $username='root';
     $password='';
-    $dbname='alumni_management_system';
+    $dbname='alumni_portal';
     $con=mysqli_connect($servername,$username,$password,$dbname);
     if(!$con){
         die('Could not Connect to MySql'.mysql_error());
